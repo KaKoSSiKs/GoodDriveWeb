a8dd5b652d476ce479227bb40a0116d611bd0704
Никитин Ярослав <nikitin19955@mail.ru>
2025-11-13 23:50:42 +0300

    resolve error build

--- mysql-init/01-init.sql
+++ mysql-init/01-init.sql
@@ -4,8 +4,8 @@
 -- Create user with mysql_native_password authentication
 CREATE USER IF NOT EXISTS 'gooddrive_user'@'%' IDENTIFIED WITH mysql_native_password BY 'gooddrive_password';
 
--- Grant all privileges on gooddrive database
-GRANT ALL PRIVILEGES ON `gooddrive`.* TO 'gooddrive_user'@'%';
+-- Grant all privileges on gooddrive_db database
+GRANT ALL PRIVILEGES ON `gooddrive_db`.* TO 'gooddrive_user'@'%';
 
 -- Flush privileges to ensure they take effect
 FLUSH PRIVILEGES;
